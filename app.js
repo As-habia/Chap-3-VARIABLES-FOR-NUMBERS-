42e8df4519a25c653cf0b4eef6200479d6b6a40e
As-habia <afaanmehmil9>
2024-05-11 13:08:46 -0700

    chapter#3

---
+++ app.js
@@ -0,0 +1,27 @@
+//task1
+var age = 23; 
+alert("My age is: " + age);
+
+
+//task2
+var visits = parseInt(localStorage.getItem('visits')) ?? 0;
+visits++;
+localStorage.setItem('visits', visits);
+alert("You have visited this site " + visits + " times.");
+
+
+//task3
+var birthYear = 1990; 
+document.write("My birth year is: " + birthYear +"<br/>");
+
+
+//task4
+var visitorName = "Bella John";
+var productTitle = "rose petals";
+var quantity = 5;
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on Bella store.");
+
+
+
+
+
